771df8c6bea3921ec70a274cce8f59e8ec9beb5f
Thierry CAROLE <thcarole1@hotmail.com>
2025-01-03 17:38:51 +0100

    prediction page DONE OK

--- app/pages/Prediction.py
+++ app/pages/Prediction.py
@@ -9,19 +9,22 @@ with open("app/css/styles.css") as f:
 
 st.markdown(
     """
-    <h1>House price prediction </h1>
+    <h1>Bank Loan prediction </h1>
     <h2> Features </h2>
 
     We have the following features : <br>
-    - **Longitude**: Geographical longitude of the block group's centroid.
-    - **Latitude**: Geographical latitude of the block group's centroid.
-    - **Housing median age**: Median age of houses in the block group.
-    - **Total rooms**: Total rooms of the block group.
-    - **Total bedrooms**: Total bedrooms of the block group.
-    - **Population**:Total population of the block group.
-    - **Households**: Total households of the block group.
-    - **Median income**: Median income of households in the block group (**in tens of thousands of dollars**).
-    - **Ocean proximity**: Proximity from ocean.
+    - **Experience**: #years of professional experience<br>
+    - **Income**: Annual income of the customer (**in thousands of dollars**) <br>
+    - **ZIP_Code**: Home Address ZIP code.<br>
+    - **Family**: Family size of the customer <br>
+    - **CCAvg**: Avg. spending on credit cards per month (**in thousands of dollars**)<br>
+    - **Education**: Education Level. 1: Undergrad; 2: Graduate; 3: Advanced/Professional<br>
+    - **Mortgage**: Value of house mortgage if any (**in thousands of dollars**).<br>
+    - **Personal Loan**: Did this customer accept the personal loan offered in the last campaign?<br>
+    - **Securities Account**: Does the customer have a securities account with the bank?<br>
+    - **CD Account**: Does the customer have a certificate of deposit (CD) account with the bank?<br>
+    - **Online**: Does the customer use internet banking facilities?<br>
+    - **CreditCard**: Does the customer use a credit card issued by UniversalBank?<br>
     """,
     unsafe_allow_html=True
 )
@@ -33,36 +36,49 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Coordinates input
+# Education, Experience and Income input
+col1, col2, col3 = st.columns(3)
+with col1:
+    options = ["Undergrad", "Graduate", "Advanced/Professional"]
+    Education = st.radio("**Education Level** :", options)
+
+    options_dict = {1 : "Undergrad", 2 : "Graduate", 3 : "Advanced/Professional"}
+    for key, value in options_dict.items():
+        if Education == value:
+            Education_encoded = key
+with col2:
+    Experience = st.number_input(label = "**#years of professional experience**", min_value=0)
+with col3:
+    Income = st.number_input(label = "**Income (in thousands of USD)**", min_value=0)
+
+# ZIP_Code and Family input
 col1, col2 = st.columns(2)
 with col1:
-    longitude = st.number_input(label = "**Longitude**")
+    ZIP_Code = st.number_input(label = "**Home Address ZIP code**", min_value=0)
 with col2:
-    latitude = st.number_input(label = "**Latitude**")
+    Family = st.number_input(label = "**Family size of the customer**", min_value=1)
 
-# House median age
-housing_median_age = st.number_input(label = "**Housing median age**", min_value=0, step=1)
+# Avg spending input
+CCAvg = st.number_input(label = "**Avg. spending on credit cards per month (**in thousands of USD**)**", min_value=0)
 
-# Census info
-col1, col2 = st.columns(2)
-with col1:
-    total_rooms = st.number_input(label = "**Total rooms**", min_value=0, step=1)
-with col2:
-    total_bedrooms = st.number_input(label="**Total bedrooms**",min_value=0, step=1)
+# Mortgage input
+Mortgage = st.number_input(label = "**Value of house mortgage if any (**in thousands of dollars**)**", min_value=0)
 
-# Population info
-col1, col2 = st.columns(2)
-with col1:
-    population = st.number_input(label = "**Population**", min_value=0, step=1)
-with col2:
-    households = st.number_input(label="**Households**",min_value=0, step=1)
+# Securities Account input
+options = [0, 1]
+Securities_Account = st.radio("**Does the customer have a securities account with the bank?**", options)
 
-# Median income
-median_income = st.number_input(label = "**Median income** (**in tens of thousands of dollars**)", min_value=0, step=1)
+# CD Account input
+options = [0, 1]
+CD_Account  = st.radio("**Does the customer have a certificate of deposit (CD) account with the bank?**", options)
 
-# Ocean proximity
-options = ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"]
-ocean_proximity = st.radio("**Ocean proximity** :", options)
+# Online input
+options = [0, 1]
+Online = st.radio("**Does the customer use internet banking facilities?**", options)
+
+# CreditCard input
+options = [0, 1]
+CreditCard  = st.radio("**Does the customer use a credit card issued by UniversalBank?**", options)
 
 st.markdown(
     """
@@ -72,15 +88,17 @@ st.markdown(
 )
 
 params = {
-            "longitude" : longitude,
-            "latitude" : latitude,
-            "housing_median_age" : housing_median_age,
-            "total_rooms" : total_rooms,
-            "total_bedrooms" : total_bedrooms,
-            "population" : population,
-            "households" : households,
-            "median_income" : median_income,
-            "ocean_proximity" : ocean_proximity
+            "Education" : Education_encoded,
+            "Experience" : Experience,
+            "Income" : Income,
+            "ZIP_Code" : ZIP_Code,
+            "Family" : Family,
+            "CCAvg" : CCAvg,
+            "Mortgage" : Mortgage,
+            "Securities_Account" : Securities_Account,
+            "CD_Account" : CD_Account,
+            "Online" : Online,
+            "CreditCard": CreditCard
          }
 
 input_df = pd.DataFrame(params, index=[0])
@@ -89,13 +107,13 @@ st.dataframe(data = input_df, hide_index=True)
 
 st.markdown(
     """
-    <h3> Prediction </h3>
+    <h3> Prediction : Is this customer likely to purchase a loan ?</h3>
     """,
     unsafe_allow_html=True
 )
 
 # API endpoint
-URL = "https://house-pricing-project-image-q7vjkyyeda-od.a.run.app/predict"
+URL = "https://bank-loan-model-image-l63mv2bm6q-od.a.run.app/predict"
 
 # Button to trigger the POST request
 if st.button("Predict"):
@@ -105,11 +123,16 @@ if st.button("Predict"):
 
         # Display the response
         if r.status_code == 200:
-            st.success(f"Response: {r.json()}")
+            # st.success(f"Response: {r.json()}")
             response = r.json()
 
-            # Get the predicted price
-            st.write(f"Predicted price : {response['prediction']['0']} USD")
+            # Get the prediction
+            if response['prediction']['0'] == 0:
+                answer = "NO"
+            else:
+                answer = "YES"
+
+            st.write(f"The answer is: {answer}")
 
             # Retrieve and display info from API
             dict = {}
